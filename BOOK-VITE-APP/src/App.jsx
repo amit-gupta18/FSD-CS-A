44c85f2c0b51b05a6207220a740bcc32c9550271
Amit Kumar Gupta <amitgupta542173@gmail.com>
2025-10-31 12:55:10 +0530

    book added

--- BOOK-VITE-APP/src/App.jsx
+++ BOOK-VITE-APP/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+// import Book from './component/Book'
+import Book from './components/Book'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import Header from './components/Header'
+const App = () => {
+  const data = [
+    { image: "https://imgs.search.brave.com/br6T4JBa6SYRgowHDWy1OAz1noHdmKgmg5ZUhxDHkHw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2NhL2Y4/LzRhL2NhZjg0YTUw/ZDVjODViN2NhM2Iz/MzQyZjc1NjNhZmQy/LmpwZw", title: "Physics", price: 765 },
+    { image: "https://imgs.search.brave.com/SdfInLRAyxZfG_0HBoluW-VN76bxyQD7kMVJrAlYb1Q/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuYm9va3NlbnNl/LmNvbS9pbWFnZXMv/MzI3LzA1Ni85Nzgw/NzQ0MDU2MzI3Lmpw/Zw", title: "Chemistry", price: 895 },
+    { image: "https://imgs.search.brave.com/jDMJbo8rNJpdDYmorSF21-UnI84faT5W5MguWUgIb2Y/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2UwLzg4/L2U1L2UwODhlNTkw/ODUzZjljNmI0NzNi/YjFjM2E0NTkwNDA0/LmpwZw", title: "Math", price: 799 }
+  ]
   return (
-    <>
-       <div>Book  Vite App</div>
-    </>
+    <div>
+      <Header />
+      <div className='app'>
+        <div className='app'>
+          {
+            data.map((b, i) => (
+              <Book key={i} book={b} />
+            ))
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
--- BOOK-VITE-APP/src/components/Book.jsx
+++ BOOK-VITE-APP/src/components/Book.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
-const Book = () => {
+import React, { useState } from 'react'
+import './Book.css'
+const Book = ({book}) => {
+    const [count,setCount] = useState(0);
+    function handleAddtoCart(){
+        alert(`${book.title} Added Successfully`)
+    }
   return (
-    <div>Book</div>
+    <div className='card'>
+      <img src={book.image} alt="book image" width={200} height={200} />
+      <h3>Title: {book.title}</h3>
+      <h4>Price: ₹{book.price}/-</h4>
+      <div>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <span>{count}</span>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+      <button onClick={handleAddtoCart}>Add to Cart</button>
+    </div>
   )
 }
 
