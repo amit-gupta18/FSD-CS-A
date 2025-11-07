18ee9d74908ee49a1760912d1ce14fb48a026c05
Amit Kumar Gupta <amitgupta542173@gmail.com>
2025-11-07 12:29:24 +0530

    added components

--- SHOPPING-VITE-APP/src/App.jsx
+++ SHOPPING-VITE-APP/src/App.jsx
@@ -3,7 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter , Routes , Route } from 'react-router-dom'
-
+import Home from '../components/Home'
+import Cart from '../components/Cart'
+import Order from '../components/Order'
+import Checkout from '../components/Checkout'
+import Login from '../components/Login'
+import Profile from '../components/Profile'
+import Register from '../components/Register'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,8 +20,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<Order />} />
-          <Route path="/register" element={<Checkout />} />
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="*" element={<Profile />} />
         </Routes>
       </BrowserRouter>
