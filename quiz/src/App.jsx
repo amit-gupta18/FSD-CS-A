6a9167f8604fb576d6e5d778b5eed4b4528b610a
Amit Kumar Gupta <amitgupta542173@gmail.com>
2026-04-08 15:27:54 +0530

    new comit

---
+++ quiz/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p className='text-2xl font-blue-500'>Testing </p>
+    </>
+  )
+}
+
+export default App
